a510180a731955c86522c85d0217310fdf5f5da9
fzed51 <fzed51@yahoo.fr>
2015-01-16 16:08:16 +0100

    :construction: ADD index + classes

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require './autoloader.class.php';
+(new autoloader())->addParentFolder()->register();
+
+$param = array_merge($_GET, $_COOKIE, $_POST);
+
+$app = new Calindex();
+
+if (isset($param['annee'], $param['mois'])){
+	$annee = (int)$param['annee'];
+	$mois  = (int)$param['mois'];
+	if($mois < 1 || $mois > 12){
+		$app->index();
+	}else{
+		$app->affiche($annee, $mois);
+	}
+}else{
+	$app->index();
+}
