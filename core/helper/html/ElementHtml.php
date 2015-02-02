c22dbcf124b871c0c7617e10df9c8aff0b2b9639
fzed51 <fzed51@yahoo.fr>
2015-02-02 23:42:30 +0100

    :construction: UPDATE Form et Html element

--- core/helper/html/ElementHtml.php
+++ core/helper/html/ElementHtml.php
@@ -13,6 +13,45 @@ namespace Core\Helper\Html;
  *
  * @author fzed51
  */
-class ElementHtml {
-    //put your code here
+class ElementHtml {	
+	/**
+	 * formate une liste d'attributs HTML
+	 * @param array $listeAttributs
+	 * @return string
+	 */
+	protected function concatAttributs(array $listeAttributs){
+		$strAttribut = array();
+		foreach ($listeAttributs as $key => $value) {
+			if(!is_bool($value)){
+				if(strlen($value) > 0){
+					$strAttribut[] = strtolower($key) . '="' . $value . '"';
+				}
+			} else {
+				if($value){
+					$strAttribut[] = strtolower($key);
+				}
+			}
+		}
+		return join(' ', $strAttribut);
+	}
+
+	protected function startElement($tag, $attrbs = array()) { 
+		$attrb = $this->concatAttributs($attrbs);
+		return "<$tag $attrb>";
+	}
+	
+	protected function endElement($tag) {
+		return "</$tag>";
+	}
+
+	protected function element($tag, $attrbs = array(), $content = ''){
+		return $this->startElement($tag, $attrbs) 
+				. $content
+				. $this->endElement($tag);
+	}
+	
+	protected function elementAutoClose($tag, $attrbs = array()){
+		$attrb = $this->concatAttributs($attrbs);
+		return "<$tag $attrb />";
+	}
 }
