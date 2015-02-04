7971ec9967ef885f06ae3f176b52083563d9d967
fzed51 <fzed51@yahoo.fr>
2015-02-04 12:05:46 +0100

    :construction: :+1:  UPDATE App et Config

--- core/AppAbstract.php
+++ core/AppAbstract.php
@@ -9,19 +9,63 @@
 namespace Core;
 
 
+use Core\Database\PDOConnect;
+use Core\Database\PDOSqLiteConnexion;
+
 abstract class AppAbstract {
     /**
      * @var string $appName Nom de l'applicatino
      */
     private $appName;
 
+    private static $Connect;
+
+
     /**
      * @param $appName
      */
-    function __construct($appName){
+    function __construct($appName, $configFile = NULL){
+
+        if(is_null($configFile)){
+            $configFile = ROOT.DS.'config'.DS.'default.conf.php';
+        }
 
         $this->appName = $appName;
+        if ($configFile != '') {
+            self::SetConfig($configFile);
+        }
 
+    }
+
+    public static function SetConfig($configFile)
+    {
+        Config::initializ($configFile);
+    }
+
+    public static function GetDB()
+    {
+        $connexion = NULL;
+        if(isset(self::$Connect)){
+            switch(strtolower(Config::get('provider'))){
+                case 'sqlite':
+                    $connexion = new PDOConnect(
+                        new PDOSqLiteConnexion(
+                            Config::get('DB_FILE')
+                        )
+                    );
+                    break;
+                case 'mysql':
+                    $connexion = new PDOConnect(
+                        new PDOSqLiteConnexion(
+                            Config::get('DB_NAME'),
+                            Config::get('DB_USER'),
+                            Config::get('DB_PASSWORD'),
+                            Config::get('DB_HOST')
+                        )
+                    );
+                    break;
+            }
+        }
     }
 
     abstract function run();
---
+++ core/config.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fabien.sanchez
+ * Date: 04/02/2015
+ * Time: 11:29
+ */
+
+namespace core;
+
+class ConfigException extends \Exception {};
+
+class ConfigNotFoundException extends ConfigException {};
+
+class Config {
+
+    static private $config;
+
+    public static function initializ($configFile){
+        if(file_exists($configFile)){
+            self::$config = require($configFile);
+        } else {
+            throw new ConfigNotFoundException($configFile);
+        }
+    }
+
+    public static function get($param, $default = NULL){
+        if (isset(self::$config[$param])){
+            return self::$config[$param];
+        }
+    }
+
+}
