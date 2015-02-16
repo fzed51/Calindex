fc44a74ebd7b97adfcd877183800f3ea0aacdd18
fzed51 <fzed51@yahoo.fr>
2015-02-16 23:31:29 +0100

    :package:  Test Date

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
+include __DIR__."\..\core\pattern\GetterSetter.php";
+include __DIR__."\..\App\Controler\Date.php";
+$d = new App\Controler\Date();
+$p = App\Controler\Date::lundi_paques($d->year);
+var_dump($d);
+var_dump($p);
 
+die();
 require (__DIR__.'/../bootstrap.php');
