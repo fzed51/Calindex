b3570e595e0e0cafe7cc8e5ac90e44455a168254
fzed51 <fzed51@users.noreply.github.com>
2015-02-24 09:18:44 +0100

    :pushpin:  RAZ sur l'index de test

--- public/index.php
+++ public/index.php
@@ -1,14 +1,3 @@
 <?php
-if( ini_get('date.timezone') == '' ){
-    date_default_timezone_set('Europe/Paris');
-}
-include __DIR__."\..\core\pattern\GetterSetter.php";
-include __DIR__."\..\App\Controler\Date.php";
-$d = new App\Controler\Date();
-$p = App\Controler\Date::lundi_paques($d->year);
 
-print ($d->format("le dd/mm/yyyy est un dddd.")."<br>");
-print ($p->format("le dd/mm/yyyy est un dddd.")."<br>");
-print ($d->format()."<br>");
-die();
 require (__DIR__.'/../bootstrap.php');
