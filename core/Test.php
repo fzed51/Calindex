5f74a454fcb03ec9983e30a1462bcdae3cbae883
fzed51 <fzed51@users.noreply.github.com>
2015-03-18 09:30:48 +0100

    :construction: UPDATE Test suite

--- core/Test.php
+++ core/Test.php
@@ -14,71 +14,62 @@ namespace Core;
  */
 class Test {
 
-protected $class_test = '';
-private $nbSuccess = 0;
-private $nbTest = 0;
+	protected $class_test = '';
+	private   $nbSuccess  = 0;
+	private   $nbTest     = 0;
 
-const ICO_OK = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\" 
-width=\"20\" height=\"20\"><metadata id=\"metadata4151\">image/svg+xml</metadata>
-<title>Layer 1</title><g id=\"layer1\"><circle fill=\"#00ff00\" stroke-width=\"40\" 
-stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-miterlimit=\"4\" 
-id=\"path4696\" cx=\"10\" cy=\"10\" r=\"8\"/><path fill=\"none\" stroke=\"#7eff7e\" 
-stroke-width=\"1\" stroke-linecap=\"round\" stroke-linejoin=\"round\" 
-stroke-miterlimit=\"4\" id=\"path4698\" d=\"m4 11a6 6 0 0 1 2-6 6 6 0 0 1 6-2\"/>
-<path fill=\"none\" fill-rule=\"evenodd\" stroke=\"#005700\" stroke-width=\"2\" 
-stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-miterlimit=\"4\" 
-d=\"m14 6c-2 2-4 5-5 8l-3-3\" id=\"path4708\"/></g></svg>";
-const ICO_KO = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\" 
-width=\"20\" height=\"20\"><metadata>image/svg+xml</metadata><title>Layer 1</title>
-<circle fill=\"#ff0000\" stroke-width=\"40\" stroke-linecap=\"round\" 
-stroke-linejoin=\"round\" stroke-miterlimit=\"4\" cx=\"10\" cy=\"10\" r=\"8\"/>
-<path fill=\"none\" stroke=\"#ff7e7e\" stroke-width=\"1\" stroke-linecap=\"round\" 
-stroke-linejoin=\"round\" stroke-miterlimit=\"4\" d=\"m4 11a6 6 0 0 1 2-6 6 6 0 0 1 6-2\"/>
-<path fill=\"none\" fill-rule=\"evenodd\" stroke=\"#ffffff\" stroke-width=\"2\" 
-stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-miterlimit=\"4\" d=\"m10 5l0 7\"/>
-<circle fill=\"#ffffff\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" 
-stroke-miterlimit=\"4\" r=\"1\" cy=\"15\" cx=\"10\"/></svg>";
+	const ICO_OK = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\"><metadata id=\"metadata4151\">image/svg+xml</metadata><title>Layer 1</title><g id=\"layer1\"><circle fill=\"#00ff00\" stroke-width=\"40\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-miterlimit=\"4\" id=\"path4696\" cx=\"10\" cy=\"10\" r=\"8\"/><path fill=\"none\" stroke=\"#7eff7e\" stroke-width=\"1\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-miterlimit=\"4\" id=\"path4698\" d=\"m4 11a6 6 0 0 1 2-6 6 6 0 0 1 6-2\"/><path fill=\"none\" fill-rule=\"evenodd\" stroke=\"#005700\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-miterlimit=\"4\" d=\"m14 6c-2 2-4 5-5 8l-3-3\" id=\"path4708\"/></g></svg>";
+	const ICO_KO = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:svg=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\"><metadata>image/svg+xml</metadata><title>Layer 1</title><circle fill=\"#ff0000\" stroke-width=\"40\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-miterlimit=\"4\" cx=\"10\" cy=\"10\" r=\"8\"/><path fill=\"none\" stroke=\"#ff7e7e\" stroke-width=\"1\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-miterlimit=\"4\" d=\"m4 11a6 6 0 0 1 2-6 6 6 0 0 1 6-2\"/><path fill=\"none\" fill-rule=\"evenodd\" stroke=\"#ffffff\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-miterlimit=\"4\" d=\"m10 5l0 7\"/><circle fill=\"#ffffff\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-miterlimit=\"4\" r=\"1\" cy=\"15\" cx=\"10\"/></svg>";
 
-function __construct() {
-$this->class_test = get_class($this);
-}
+	function __construct() {
+		$this->class_test = get_class($this);
+	}
 
-function run() {
-?>
-<div class="test_class">
-    <h3>Test de la class <span class="class_name"><?= $this->class_test; ?></span></h3>
-    <?php
-    $methodes = get_class_methods($this->class_test);
-    foreach ($methodes as $methode) {
-    if (substr($methode, 0, 6) == 'test__'){
-    $methode_name = substr($methode, 6);
-    ob_start();
-    $succes = call_user_func(array($this, $methode));
-    $comment = ob_get_contents();
-    ob_end_clean();    
-    ?>
-    <div class="test_methode <?= ($succes)?"ok":"ko"; ?>">
-        <p>Test de la methode <span class="methode_name"><?= $methode_name; ?></span></p>
-        <div class="test_methode_comment"><?= $comment; ?></div>
-    </div>
-    <?php
-    }
-    }
-    ?>
-</div>
-<?php
-}
+	function run() {
+		?>
+		<div class="test_class">
+			<h3>Test de la class <span class="class_name"><?= $this->class_test; ?></span></h3>
+			<?php
+			$methodes = get_class_methods($this->class_test);
+			foreach ($methodes as $methode) {
+				if (substr($methode, 0, 6) == 'test__') {
+					$methode_name = substr($methode, 6);
+					ob_start();
+					$succes = call_user_func(array($this, $methode));
+					$comment = ob_get_contents();
+					ob_end_clean();
+					?>
+					<div class="test_methode <?= ($succes) ? "ok" : "ko"; ?>">
+						<div class="test_methode_resume ihiddable"><?= $this->getPourcent();?></div>
+						<p>Test de la methode <span class="methode_name"><?= $methode_name; ?></span></p>
+						<div class="test_methode_comment hiddable"><?= $comment; ?></div>
+					</div>
+					<?php
+				}
+			}
+			?>
+		</div>
+		<?php
+	}
 
-function testEgal($elementTest, $elementComparaison, $libelle) {
-$succes = ($elementTest == $elementComparaison);
-$this->nbTest++;
-if ($succes) {
-    $this->nbSuccess++;
-    echo "<p>".self::ICO_OK.nl2br($libelle)."</p>";
-}else{
-    echo "<p>".self::ICO_KO.nl2br($libelle)."</p>";
-}
-return $succes;
-}
+	function testEgal($elementTest, $elementComparaison, $libelle) {
+		$succes = ($elementTest == $elementComparaison);
+		$this->nbTest++;
+		if ($succes) {
+			$this->nbSuccess++;
+			echo "<p>" . self::ICO_OK . nl2br($libelle) . "</p>";
+		} else {
+			echo "<p>" . self::ICO_KO . nl2br($libelle) . "</p>";
+		}
+		return $succes;
+	}
+
+	function getPourcent() {
+		if ($this->nbTest == 0) {
+			return "****%";
+		} else {
+			return round(100 * $this->nbSuccess / $this->nbTest, 1) . "%";
+		}
+	}
 
 }
--- test.php
+++ test.php
@@ -1,6 +1,5 @@
 <?php
-
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,127 +8,138 @@
 use Core\Autoloader;
 use Core\Test;
 
-date_default_timezone_set ('Europe/Paris');
+date_default_timezone_set('Europe/Paris');
 
-define ('DS', DIRECTORY_SEPARATOR);
-define ('WS', '/');
-define ('ROOT', __DIR__);
-define ('ROOT_VUE', ROOT . DS . 'app' . DS . 'vue');
-define ('WEBROOT', 'localhost');
+define('DS', DIRECTORY_SEPARATOR);
+define('WS', '/');
+define('ROOT', __DIR__);
+define('ROOT_VUE', ROOT . DS . 'app' . DS . 'vue');
+define('WEBROOT', 'localhost');
 
-require ROOT.DS.'core'.DS.'autoloader.class.php';
+require ROOT . DS . 'core' . DS . 'autoloader.class.php';
 $autoloader = new Autoloader();
 $autoloader->activeCache(true)
-    ->addExtension('.php')
-    ->addExtension('.class.php')
-    ->addFolder(ROOT, true)
-    ->addFolder(ROOT.DS.'lib', true)
-    ->register();
+				->addExtension('.php')
+				->addExtension('.class.php')
+				->addFolder(ROOT, true)
+				->addFolder(ROOT . DS . 'lib', true)
+				->register();
 
-
-function array_union(&$array1, $array2){
-    foreach ($array2 as $value) {
-        array_push($array1, $value);
-    }
+function array_union(&$array1, $array2) {
+	foreach ($array2 as $value) {
+		array_push($array1, $value);
+	}
 }
 
-function scanFolder($folder){
-    $lstFileTest = [];
-    $lstFSO = scandir($folder);
-    $bad = ['.', '..'];
-    foreach ($lstFSO as $fso) {
-        if (!in_array($fso, $bad)){
-            $fsoFull = $folder.DS.$fso;
-            if(is_dir($fsoFull)){
-                scanFolder($fsoFull);
-            }elseif (is_file($fsoFull)) {
-                if (substr($fso, 0, 6) == 'test__' && substr($fso, -4) == '.php'){
-                    echo "<p>$fsoFull</p>\n";
-                    require $fsoFull;
-                }
-            }
-        }
-    }
+function scanFolder($folder) {
+	$lstFileTest = [];
+	$lstFSO = scandir($folder);
+	$bad = ['.', '..'];
+	foreach ($lstFSO as $fso) {
+		if (!in_array($fso, $bad)) {
+			$fsoFull = $folder . DS . $fso;
+			if (is_dir($fsoFull)) {
+				scanFolder($fsoFull);
+			} elseif (is_file($fsoFull)) {
+				if (substr($fso, 0, 6) == 'test__' && substr($fso, -4) == '.php') {
+					echo "<p>$fsoFull</p>\n";
+					require $fsoFull;
+				}
+			}
+		}
+	}
 }
 
 function runTest(array $folders) {
+	?><!DOCTYPE html>
+	<html lang="fr">
+		<head>
+			<meta charset="utf-8" />
+			<title> Test Suite </title>
+			<style>
+				body{
+					font-family: sans-serif;
+					font-size: 100%
+				}
+				*>.hiddable{
+					display: none;
+				}
+				*:hover>.hiddable{
+					display: block;
+				}
+				*>.ihiddable{
+					display: block;
+				}
+				*:hover>.ihiddable{
+					display: none;
+				}
+				.section{
+					border-radius: 10px;
+					border: 1px solid #000;
+					background-color: #ddd;
+					max-width: 1024px;
+					margin: 10px auto;
+					padding: 5px;
+				}
+				.fichiers{}
+				.fichiers .fichiers_liste{}
+				.tests{}
+				.tests .tests_liste{}
+				.tests .class_name{
+					font-weight: bold;
+				}
+				.tests .methode_name{}
+				.tests .test_methode{}
+				.tests .test_methode p{
+					line-height: 20px;
+				}
+				.ok{
+					background-color: rgba(0,255,0,0.1); 
+				}
+				.ko{
+					background-color: rgba(255,0,0,0.1); 
+				}
+				.tests .test_methode_comment{}
+				.tests .test_methode_resume{
+					float:right;
+				}
+				.tests .test_class{}
+			</style>
+		</head>
+		<body>
+			<h1>Test suite</h1>
+			<div class="section fichiers">
+				<h2>Fichiers chargés</h2>
+				<div class="hiddable fichiers_liste">
+					<?php
+					// charge tous les fichiers de test
+					foreach ($folders as $folder) {
+						$folder = __DIR__ . DS . $folder;
+						if (is_dir($folder)) {
+							scanFolder($folder);
+						}
+					}
+					?>
+				</div>
+			</div>
+			<div class="section tests">
+				<h2>Résultats des test</h2>
+				<div class="tests_liste">
+					<?php
+					// recherche des class de test
 
-    ?><!DOCTYPE html>
-<html lang="fr">
-<head>
-<meta charset="utf-8" />
-<title> Test Suite </title>
-<style>
-    .section{
-        border-radius: 10px;
-        border: 1px solid #000;
-        background-color: #ddd;
-        width: 1024px;
-        margin: 10px auto;
-        padding: 5px;
-    }
-    .section .hiddable{
-        display: none;
-    }
-    .section:hover .hiddable{
-        display: block;
-    }
-    .fichiers{}
-    .fichiers .fichiers_liste{}
-    .tests{}
-    .tests .tests_liste{}
-    .tests .class_name{
-        font-weight: bold;
-    }
-    .tests .methode_name{}
-    .tests .test_methode{}
-    .tests .test_methode p{
-        line-height: 20px;
-    }
-    .ok{
-        background-color: rgba(0,255,0,0.1); 
-    }
-    .ko{
-        background-color: rgba(255,0,0,0.1); 
-    }
-    .tests .test_methode_comment{}
-    .tests .test_class{}
-</style>
-</head>
-<body>
-    <h1>Test suite</h1>
-    <div class="section fichiers">
-        <h2>Fichiers chargés</h2>
-        <div class="hiddable fichiers_liste">
-    <?php
-    // charge tous les fichiers de test
-    foreach ($folders as $folder) {
-        $folder = __DIR__.DS.$folder;
-        if (is_dir($folder)) {
-            scanFolder($folder);
-        }
-    }
-    ?>
-        </div>
-    </div>
-    <div class="section tests">
-        <h2>Résultats des test</h2>
-        <div class="tests_liste">
-    <?php
-    // recherche des class de test
+					$liste_class_chargee = get_declared_classes();
+					foreach ($liste_class_chargee as $class) {
+						if (is_subclass_of($class, 'Core\Test')) {
+							$test = new $class();
+							$test->run();
+						}
+					}
+					?>
+				</div>
+			</div>
+			<?php
+		}
 
-    $liste_class_chargee = get_declared_classes();
-    foreach ($liste_class_chargee as $class) {
-        if (is_subclass_of($class, 'Core\Test')){
-            $test = new $class();
-            $test->run();
-        }
-    }
-    ?>
-        </div>
-    </div>
-    <?php
-        
-}
-
-runTest(['core', 'app']);
+		runTest(['core', 'app']);
+		
