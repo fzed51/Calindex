aaee3fd9de450d97cc0198784e92efac8a27ca0a
fzed51 <fzed51@users.noreply.github.com>
2015-03-18 15:34:35 +0100

    :construction: UPDATE Date test

--- app/controler/test__Date.class.php
+++ app/controler/test__Date.class.php
@@ -14,26 +14,37 @@ class test__Date extends \Core\Test {
         $s = true;
         
         $oDate1 = new Date();
-        $oDate2 = new Date('2014');
-        $oDate3 = new Date('201402');
-        $oDate4 = new Date('20140220');
         $date = date('Ymd');
-        
         $s = $this->testEgal($oDate1, $date, "__construct sans paramètres et __toString") && $s;
+		
+        $oDate2 = new Date('2014');
         $s = $this->testEgal($oDate2, "20140101", "__construct avec l'année en paramètre") && $s;
+		
+		
+        $oDate3 = new Date('201402');
         $s = $this->testEgal($oDate3, "20140201", "__construct avec l'année et le mois en paramètre") && $s;
+		
+        $oDate4 = new Date('20140220');
         $s = $this->testEgal($oDate4, "20140220", "__construct avec une date en paramètre") && $s;
         
         return $s;
     }
 
     function test__add_day(){
+		$s = true;
 
         $oDate = new Date('20140220');
         $oDate->add_day(90);
-
-        $s = true;
-        $s = $this->testEgal($oDate, '20140521', 'add_day 90 jours') && $s;
+        $s = $this->testEgal($oDate,   '20140521', 'add_day 90 jours') && $s;
+		
+		$oDate2 = new Date('20140220');
+        $oDate2c = $oDate2->add_day(90, true);
+		$s = $this->testEgal($oDate2,  '20140220', 'add_day avec clone, date d\'origine inchangée') && $s;
+		$s = $this->testEgal($oDate2c, '20140521', 'add_day avec clone, control du clone') && $s;
+		
+		$oDate3 = new Date('20140220');
+        $oDate3->add_day(4000);
+		$s = $this->testEgal($oDate3,  '20250202', 'add_day 4000 jours') && $s;
 
         return $s; 
     }
