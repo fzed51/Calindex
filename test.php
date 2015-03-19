5b128bdb0fddf9e4705320e15edbe5af23dbe0ae
fzed51 <fzed51@users.noreply.github.com>
2015-03-19 09:06:20 +0100

    :eyes:  :construction:  White space & UPDATE Test suite

--- test.php
+++ test.php
@@ -19,11 +19,11 @@ define('WEBROOT', 'localhost');
 require ROOT . DS . 'core' . DS . 'autoloader.class.php';
 $autoloader = new Autoloader();
 $autoloader->activeCache(true)
-				->addExtension('.php')
-				->addExtension('.class.php')
-				->addFolder(ROOT, true)
-				->addFolder(ROOT . DS . 'lib', true)
-				->register();
+		->addExtension('.php')
+		->addExtension('.class.php')
+		->addFolder(ROOT, true)
+		->addFolder(ROOT . DS . 'lib', true)
+		->register();
 
 function array_union(&$array1, $array2) {
 	foreach ($array2 as $value) {
@@ -52,95 +52,95 @@ function scanFolder($folder) {
 
 function runTest(array $folders) {
 	?><!DOCTYPE html>
-<html lang="fr">
-	<head>
-		<meta charset="utf-8" />
-		<title> Test Suite </title>
-		<style>
-			body{
-				font-family: sans-serif;
-				font-size: 100%
-			}
-			*>.hiddable{
-				display: none;
-			}
-			*:hover>.hiddable{
-				display: block;
-			}
-			*>.ihiddable{
-				display: block;
-			}
-			*:hover>.ihiddable{
-				display: none;
-			}
-			.section{
-				border-radius: 10px;
-				border: 1px solid #000;
-				background-color: #ddd;
-				max-width: 1024px;
-				margin: 10px auto;
-				padding: 5px;
-			}
-			.fichiers{}
-			.fichiers .fichiers_liste{}
-			.tests{}
-			.tests .tests_liste{}
-			.tests .class_name{
-				font-weight: bold;
-			}
-			.tests .methode_name{}
-			.tests .test_methode{}
-			.tests .test_methode p{
-				line-height: 20px;
-			}
-			.ok{
-				background-color: rgba(0,255,0,0.1); 
-			}
-			.ko{
-				background-color: rgba(255,0,0,0.1); 
-			}
-			.tests .test_methode_comment{}
-			.tests .test_methode_resume{
-				float:right;
-			}
-			.tests .test_class{}
-			.tests .test_class_resume{}
-		</style>
-	</head>
-	<body>
-		<h1>Test suite</h1>
-		<div class="section fichiers">
-			<h2>Fichiers chargés</h2>
-			<div class="hiddable fichiers_liste">
-	<?php
-	// charge tous les fichiers de test
-	foreach ($folders as $folder) {
-		$folder = __DIR__ . DS . $folder;
-		if (is_dir($folder)) {
-			scanFolder($folder);
-		}
-	}
-	?>
+	<html lang="fr">
+		<head>
+			<meta charset="utf-8" />
+			<title> Test Suite </title>
+			<style>
+				body{
+					font-family: sans-serif;
+					font-size: 100%
+				}
+				*>.hiddable{
+					display: none;
+				}
+				*:hover>.hiddable{
+					display: block;
+				}
+				*>.ihiddable{
+					display: block;
+				}
+				*:hover>.ihiddable{
+					display: none;
+				}
+				.section{
+					border-radius: 10px;
+					border: 1px solid #000;
+					background-color: #ddd;
+					max-width: 1024px;
+					margin: 10px auto;
+					padding: 5px;
+				}
+				.fichiers{}
+				.fichiers .fichiers_liste{}
+				.tests{}
+				.tests .tests_liste{}
+				.tests .class_name{
+					font-weight: bold;
+				}
+				.tests .methode_name{}
+				.tests .test_methode{}
+				.tests .test_methode p{
+					line-height: 20px;
+				}
+				.ok{
+					background-color: rgba(0,255,0,0.1);
+				}
+				.ko{
+					background-color: rgba(255,0,0,0.1);
+				}
+				.tests .test_methode_comment{}
+				.tests .test_methode_resume{
+					float:right;
+				}
+				.tests .test_class{}
+				.tests .test_class_resume{}
+			</style>
+		</head>
+		<body>
+			<h1>Test suite</h1>
+			<div class="section fichiers">
+				<h2>Fichiers chargés</h2>
+				<div class="hiddable fichiers_liste">
+					<?php
+					// charge tous les fichiers de test
+					foreach ($folders as $folder) {
+						$folder = __DIR__ . DS . $folder;
+						if (is_dir($folder)) {
+							scanFolder($folder);
+						}
+					}
+					?>
+				</div>
 			</div>
-		</div>
-		<div class="section tests">
-			<h2>Résultats des test</h2>
-			<div class="tests_liste">
-	<?php
-	// recherche des class de test
+			<div class="section tests">
+				<h2>Résultats des test</h2>
+				<div class="tests_liste">
+					<?php
+					// recherche des class de test
 
-	$liste_class_chargee = get_declared_classes();
-	foreach ($liste_class_chargee as $class) {
-		if (is_subclass_of($class, 'Core\Test')) {
-			$test = new $class();
-			$test->run();
-		}
-	}
-	?>
+					$liste_class_chargee = get_declared_classes();
+					foreach ($liste_class_chargee as $class) {
+						if (is_subclass_of($class, 'Core\Test')) {
+							$test = new $class();
+							$test->run();
+						}
+					}
+					?>
+				</div>
 			</div>
-		</div>
-	</body>
-</html>
+		</body>
+	</html>
 	<?php
 }
 
@@ -149,16 +149,25 @@ $profile = function () use(&$tab) {
 	$backtraces = debug_backtrace();
 	//array_push($tab, [ 'file' => $backtrace[0]['file'], 'line'=> $backtrace[0]['line'] );
 	foreach ($backtraces as $backtrace) {
-		$file = $backtrace['file'];
-		$line = $backtrace['line'];
-		if(!isset($tab[$file])){
+		if (isset($backtrace['file'])) {
+			$file = $backtrace['file'];
+		} else {
+			$file = '?';
+		}
+		if (isset($backtrace['file'])) {
+			$line = $backtrace['line'];
+		} else {
+			$line = '?';
+		}
+		if (!isset($tab[$file])) {
 			$tab[$file] = [];
 		}
-		$tab[$file][(string)$line]=true;
+		$tab[$file][(string) $line] = true;
 	}
 };
+declare(ticks = 1);
 register_tick_function($profile);
-declare(ticks=1);
 
 runTest(['core', 'app']);
-		
+
+file_put_contents(ROOT . DS . 'utilisation_fichier.txt', serialize($tab));
