e2072fc3cc6a90f1864262cbfa2f304f9adcac4e
fzed51 <fzed51@yahoo.fr>
2015-03-20 05:22:31 +0100

    :eyes: UPDATE white space

--- bootstrap.php
+++ bootstrap.php
@@ -3,31 +3,31 @@
 use App\App;
 use Core\Autoloader;
 
-date_default_timezone_set ('Europe/Paris');
+date_default_timezone_set('Europe/Paris');
 
 define('DS', DIRECTORY_SEPARATOR);
 define('WS', '/');
 define('ROOT', __DIR__ . DS);
 define('ROOT_VUE', ROOT . 'app' . DS . 'vue');
 $directory = basename(ROOT);
-$tabUrl = explode($directory, filter_input(INPUT_SERVER , 'REQUEST_URI', FILTER_SANITIZE_URL));
+$tabUrl = explode($directory, filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL));
 if (count($tabUrl) > 1) {
-	define('WEBROOT', $tabUrl[0] . $directory . WS);
+    define('WEBROOT', $tabUrl[0] . $directory . WS);
 } else {
-	define('WEBROOT', WS);
+    define('WEBROOT', WS);
 }
 
-require ROOT.DS.'core'.DS.'autoloader.class.php';
+require ROOT . DS . 'core' . DS . 'autoloader.class.php';
 $autoloader = new Autoloader();
 $autoloader->activeCache(true)
-    ->addExtension('.php')
-    ->addExtension('.class.php')
-    ->addFolder(ROOT, true)
-    ->addFolder(ROOT.DS.'lib', true)
-    ->register();
+        ->addExtension('.php')
+        ->addExtension('.class.php')
+        ->addFolder(ROOT, true)
+        ->addFolder(ROOT . DS . 'lib', true)
+        ->register();
 
 (new App('Mon site'))
-    ->run();
+        ->run();
 
 // utile pour être sur de passer dans le destructeur de l'autoloader.
 unset($autoloader);
