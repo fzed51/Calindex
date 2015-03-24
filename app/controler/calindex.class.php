61a7c6cde77ca50182f6fed1421b63dafc2e5073
fzed51 <fzed51@users.noreply.github.com>
2015-03-24 16:20:37 +0100

    :eyes: UPDATEFormat

--- app/controler/calindex.class.php
+++ app/controler/calindex.class.php
@@ -6,39 +6,38 @@ use App\Vue\Vue;
 use App\Controler\Date;
 
 class Calindex {
-	
-	function index(){
+
+	function index() {
 		$now = new Date();
 		$this->affiche($now->year, $now->month);
 	}
-	
+
 	private function premier_du_mois($annee, $mois, $modif = 0) {
 		$date = Date::make($annee, $mois, 1);
-		$modif = (int)$modif;
-		if($modif != 0){
+		$modif = (int) $modif;
+		if ($modif != 0) {
 			$date->add_month($modif);
 		}
 		return $date;
 	}
-	
-	function affiche($annee, $mois){
+
+	function affiche($annee, $mois) {
 		$vue = new Vue('vue_3_mois');
-		
+
 		$oMoisAvant = new Calendrier($this->premier_du_mois($annee, $mois, -1));
-		$oMois      = new Calendrier($this->premier_du_mois($annee, $mois));
+		$oMois = new Calendrier($this->premier_du_mois($annee, $mois));
 		$oMoisApres = new Calendrier($this->premier_du_mois($annee, $mois, 1));
-		
+
 		$mois_precedant = $oMoisAvant->getData();
-		$mois_en_cours  = $oMois->getData();
-		$mois_suivant   = $oMoisApres->getData();
-		
+		$mois_en_cours = $oMois->getData();
+		$mois_suivant = $oMoisApres->getData();
+
 		$data = compact('mois_precedant', 'mois_en_cours', 'mois_suivant');
-		echo $vue->render($data);		
+		echo $vue->render($data);
 	}
 
-	function newEventPermanent($annee, $mois, $jour, $param=[]){
+	function newEventPermanent($annee, $mois, $jour, $param = []) {
 		$vue = new Vue('nouveau_evenement_permanent');
-
 	}
-	
+
 }
