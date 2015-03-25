1e9fbb7d5ec3e94f90c0c26c54f5a7ee1d5e5cf8
fzed51 <fzed51@yahoo.fr>
2015-03-25 01:22:21 +0100

    :construction: UPDATE Calendrier
    Test pour le téléchargement et la lecture des calendrier de vacances
    scolaire.

--- app/controler/calendrier.class.php
+++ app/controler/calendrier.class.php
@@ -11,104 +11,109 @@ use App\Controler\Date;
  */
 class Calendrier {
 
-    public $annee;
-    public $mois;
-    public $evenement_perpetuel;
-    public $evenement_normal;
-    public $jours_ferier;
+	public $annee;
+	public $mois;
+	public $evenement_perpetuel;
+	public $evenement_normal;
+	public $jours_ferier;
 
-    function __construct(Date $premierJour) {
-        $this->annee = $premierJour->year;
-        $this->mois = $premierJour->month;
-        $this->evenement_perpetuel = array();
-        $this->evenement_normal = array();
-    }
+	function __construct(Date $premierJour) {
+		$this->annee = $premierJour->year;
+		$this->mois = $premierJour->month;
+		$this->evenement_perpetuel = array();
+		$this->evenement_normal = array();
+	}
 
-    function getData() {
-        $this->make_evenement_perpetuel();
-        $this->make_evenement_normal();
-        $this->make_jours_ferier();
-        $this->make_vacances();
-        $infos = $this->getInfos();
-        $jours = $this->getJours();
-        return compact('infos', 'jours');
-    }
+	function getData() {
+		$this->make_evenement_perpetuel();
+		$this->make_evenement_normal();
+		$this->make_jours_ferier();
+		$this->make_vacances();
+		$infos = $this->getInfos();
+		$jours = $this->getJours();
+		return compact('infos', 'jours');
+	}
 
-    private function make_evenement_perpetuel() {
-        //$events = EvenementPermanent::getMonth($this->mois);
-    }
+	private function make_evenement_perpetuel() {
+		//$events = EvenementPermanent::getMonth($this->mois);
+	}
 
-    private function make_evenement_normal() {
-        
-    }
+	private function make_evenement_normal() {
 
-    private function make_jours_ferier() {
-        if (count($this->jours_ferier) == 0) {
-            $annee = $this->annee;
-            $feries = [];
-            $feries[] = Date::make($annee, 01, 01);
-            $feries[] = Date::make($annee, 05, 01);
-            $feries[] = Date::make($annee, 05, 08);
-            $feries[] = Date::make($annee, 07, 14);
-            $feries[] = Date::make($annee, 08, 15);
-            $feries[] = Date::make($annee, 11, 01);
-            $feries[] = Date::make($annee, 11, 11);
-            $feries[] = Date::make($annee, 12, 24);
-            $feries[] = Date::lundi_paques($annee);
-            $feries[] = Date::ascension($annee);
-            $feries[] = Date::pentecote($annee);
-            $this->jours_ferier = $feries;
-        }
-    }
-    
-    private function est_jours_ferier($oJour) {
-        foreach ($this->jours_ferier as $ferier) {
-            
-            if ($oJour->compare($ferier)==0){
-                return true;
-            }
-        }
-        return false;
-    }
+	}
 
-    private function make_vacances() {
-        
-    }
+	private function make_jours_ferier() {
+		if (count($this->jours_ferier) == 0) {
+			$annee = $this->annee;
+			$feries = [];
+			$feries[] = Date::make($annee, 01, 01);
+			$feries[] = Date::make($annee, 05, 01);
+			$feries[] = Date::make($annee, 05, 08);
+			$feries[] = Date::make($annee, 07, 14);
+			$feries[] = Date::make($annee, 08, 15);
+			$feries[] = Date::make($annee, 11, 01);
+			$feries[] = Date::make($annee, 11, 11);
+			$feries[] = Date::make($annee, 12, 24);
+			$feries[] = Date::lundi_paques($annee);
+			$feries[] = Date::ascension($annee);
+			$feries[] = Date::pentecote($annee);
+			$this->jours_ferier = $feries;
+		}
+	}
 
-    private function getInfos() {
-        $annee = $this->annee;
-        $mois = $this->mois;
-        $date = Date::make($annee, $mois, 1);
-        $nom_mois = $date->format('mmmm');
-        return compact('annee', 'mois', 'nom_mois');
-    }
+	private function est_jours_ferier($oJour) {
+		foreach ($this->jours_ferier as $ferier) {
 
-    private function getJours() {
-        $jours = array();
-        for (   $oJour = Date::make($this->annee, $this->mois, 1); 
-                $oJour->month == $this->mois; 
-                $oJour->add_day(1)) {
-            $jours[$oJour->format()] = $this->getJour($oJour);
-        }
-        return $jours;
-    }
+			if ($oJour->compare($ferier) == 0) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    private function getJour(Date $oJour) {
-        $now = new Date();
-        $jour = $oJour->day;
-        $numJour = (int) $oJour->format('j');
-        $abr_jour = $oJour->format('ddd');
-        $WE = ($numJour == 6 or $numJour == 7);
-        $aujourdhui = ($oJour->compare($now)) == 0;
-        $ferier = $this->est_jours_ferier($oJour);
-        $eventPermanent = [];
-        $eventNormal = [];
-        $vacancesZA = false;
-        $vacancesZB = false;
-        $vacancesZC = false;
-        return compact('jour', 'numJour', 'abr_jour', 'WE', 'aujourdhui', 'ferier', 
-                'eventPermanent', 'eventNormal', 'vacancesZA', 'vacancesZB', 
-                'vacancesZC');
-    }
+	private function make_vacances() {
+		$ch = curl_init();
+		$source = 'http://www.education.gouv.fr/download.php?file=http%3A%2F%2Fcache.media.education.gouv.fr%2Fics%2FCalendrier_Scolaire_Zones_A_B_C.ics&submit=';
+		curl_setopt($ch, CURLOPT_URL, $source);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		$destination = ROOT_TMP . "cacheVacances.ics";
+		$file = fopen($destination, "w+");
+		fputs($file, $data);
+		fclose($file);
+	}
+
+	private function getInfos() {
+		$annee = $this->annee;
+		$mois = $this->mois;
+		$date = Date::make($annee, $mois, 1);
+		$nom_mois = $date->format('mmmm');
+		return compact('annee', 'mois', 'nom_mois');
+	}
+
+	private function getJours() {
+		$jours = array();
+		for ($oJour = Date::make($this->annee, $this->mois, 1); $oJour->month == $this->mois; $oJour->add_day(1)) {
+			$jours[$oJour->format()] = $this->getJour($oJour);
+		}
+		return $jours;
+	}
+
+	private function getJour(Date $oJour) {
+		$now = new Date();
+		$jour = $oJour->day;
+		$numJour = (int) $oJour->format('j');
+		$abr_jour = $oJour->format('ddd');
+		$WE = ($numJour == 6 or $numJour == 7);
+		$aujourdhui = ($oJour->compare($now)) == 0;
+		$ferier = $this->est_jours_ferier($oJour);
+		$eventPermanent = [];
+		$eventNormal = [];
+		$vacancesZA = false;
+		$vacancesZB = false;
+		$vacancesZC = false;
+		return compact('jour', 'numJour', 'abr_jour', 'WE', 'aujourdhui', 'ferier', 'eventPermanent', 'eventNormal', 'vacancesZA', 'vacancesZB', 'vacancesZC');
+	}
 
 }
--- bootstrap.php
+++ bootstrap.php
@@ -8,26 +8,27 @@ date_default_timezone_set('Europe/Paris');
 define('DS', DIRECTORY_SEPARATOR);
 define('WS', '/');
 define('ROOT', __DIR__ . DS);
-define('ROOT_VUE', ROOT . 'app' . DS . 'vue');
+define('ROOT_TMP', ROOT . 'tmp' . DS);
+define('ROOT_VUE', ROOT . 'app' . DS . 'vue' . DS);
 $directory = basename(ROOT);
 $tabUrl = explode($directory, filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL));
 if (count($tabUrl) > 1) {
-    define('WEBROOT', $tabUrl[0] . $directory . WS);
+	define('WEBROOT', $tabUrl[0] . $directory . WS);
 } else {
-    define('WEBROOT', WS);
+	define('WEBROOT', WS);
 }
 
 require ROOT . DS . 'core' . DS . 'autoloader.class.php';
 $autoloader = new Autoloader();
 $autoloader->activeCache(true)
-        ->addExtension('.php')
-        ->addExtension('.class.php')
-        ->addFolder(ROOT, true)
-        ->addFolder(ROOT . DS . 'lib', true)
-        ->register();
+		->addExtension('.php')
+		->addExtension('.class.php')
+		->addFolder(ROOT, true)
+		->addFolder(ROOT . DS . 'lib', true)
+		->register();
 
 (new App('Mon site'))
-        ->run();
+		->run();
 
 // utile pour être sur de passer dans le destructeur de l'autoloader.
 unset($autoloader);
