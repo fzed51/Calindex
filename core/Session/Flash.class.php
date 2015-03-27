5895e31f2616aadf943a76be92fdd38b87c6b2d4
fzed51 <fzed51@users.noreply.github.com>
2015-03-27 09:39:11 +0100

    Revert ":construction: Correction des conflit"
    This reverts commit 91fb1dc7a54c79ba9159a476848c4ebabb37db5a.

--- core/Session/Flash.class.php
+++ core/Session/Flash.class.php
@@ -1,75 +1,50 @@
 <?php
-
 namespace Core\Session;
 
-class FlashException extends Exception {
+class FlashException extends Exception {}
 
-}
+class FlashUnknowException extends FlashException {}
 
-class FlashUnknowException extends FlashException {
-
-}
-
-class Flash {
-
+class Flash 
+{
+	
 	use Core\Pattern\Singleton;
 
 	const IDSESSION = "__SESSION__FLASH__";
-
 	private $Session;
 	private $layoutMaster;
 	private $layoutFlash = [];
 	private $defaultType;
 
-	protected function __construct() {
+	protected function __construct(){
 		$this->Session = Session::getInstance();
-		if (!isset($this->Session[self::IDSESSION]) || !is_array($this->Session[self::IDSESSION])) {
-			$this->Session[self::IDSESSION] = [];
+		if (!isset($this->Session[IDSESSION]) || !is_array($this->Session[IDSESSION])){
+			$this->Session[IDSESSION] = [];
 		}
 	}
 
-	protected function setLayout($layout) {
+	protected function setLayout($layout){
 		$this->layoutMaster = $layout;
 	}
 
-	protected function setLayoutFlash($type, $layout) {
+	protected function setLayoutFlash($type, $layout){
 		$this->layoutFlash[$type] = $layout;
 	}
 
-	protected function setDefaultType($type) {
-		if (!isset($this->layoutFlash[$type])) {
+	protected function setDefaultType($type){
+		if(!isset($this->layoutFlash[$type])){
 			throw new FlashUnknowException("Le type Flash $type n'existe pas.");
 		}
 		$this->defaultType = $type;
 	}
 
-	public function set($message, $type = '') {
-		if (empty($type)) {
-			$type = $this->defaultType;
-		}
-		array_push($this->Session[self::IDSESSION], ["type" => $type, "message" => $message]);
+	public function set($message, $type = ''){
+		if(empty($type)){$type = $this->defaultType;}
+		array_push($this->Session[IDSESSION], ["type"=>$type, "message"=> $message])
 	}
 
-	public function __toString() {
-		$content = "";
-		foreach ($this->Session[self::IDSESSION] as $flash) {
-			$content .= str_replace('{$message}', $flash['message'], str_replace('{$type}', $flash['type'], $this->layoutFlash));
-		}
-		$toString = str_replace('{$content}', $content, $this->layoutMaster);
-		$this->raz();
-		return $toString;
-	}
+	public function __toString(){
+		
+	} 
 
-	public function render() {
-		return $this->__toString();
-	}
-
-	public function affiche() {
-		echo $this->__toString();
-	}
-
-	public function raz() {
-		unset($this->Session[self::IDSESSION]);
-	}
-
-}
+} 
