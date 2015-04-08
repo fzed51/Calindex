579b8102d2b58912256b2cd0bd27e89190fb2aa8
fzed51 <fzed51@users.noreply.github.com>
2015-04-08 12:06:00 +0200

    :up: Modification du routeur pour renvoyer le bon mime type

--- public/routeur.php
+++ public/routeur.php
@@ -8,18 +8,42 @@
 
 // router.php
 if (php_sapi_name() == 'cli-server') {
-
-	if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/public" . $_SERVER["REQUEST_URI"])) {
-		return false;
+	$fullNamePublic = $_SERVER["DOCUMENT_ROOT"] . "/public" . $_SERVER["REQUEST_URI"];
+	if (file_exists($fullNamePublic)) {
+		$path = pathinfo($fullNamePublic);
+		switch ($path["extension"]) {
+			case 'ico':
+				header("Content-Type: image/x-icon");
+				break;
+			case 'css':
+				header("Content-Type: text/css");
+				break;
+			case 'js':
+				header("Content-Type: application/javascript");
+				break;
+			case 'json':
+				header("Content-Type: application/json");
+				break;
+			case 'xml':
+				header("Content-Type: application/xml");
+				break;
+			case 'jpg':
+				header("Content-Type: image/jpeg");
+				break;
+			case 'png':
+				header("Content-Type: image/png");
+				break;
+			case 'gif':
+				header("Content-Type: image/gif");
+				break;
+			case 'svg':
+				header("Content-Type: image/svg+xml");
+				break;
+			default:
+				return FALSE;
+		}
+		readfile($fullNamePublic);
 	} else {
 		require "./public/index.php";
 	}
-
-//	$path = pathinfo($_SERVER["SCRIPT_FILENAME"]);
-//	if ($path["extension"] == "el") {
-//		header("Content-Type: text/x-script.elisp");
-//		readfile($_SERVER["SCRIPT_FILENAME"]);
-//	} else {
-//		return FALSE;
-//	}
 }
