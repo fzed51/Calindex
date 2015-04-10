321716d1663514dad6aca146ba49b8972edf26d0
fzed51 <fzed51@yahoo.fr>
2015-04-11 00:37:36 +0200

    :+1::up: création des élément tache et carte

---
+++ app/controler/ICal/Card.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Controler\ICal;
+
+/**
+ * Description of Card
+ *
+ * @author fzed51
+ */
+class Card {
+	//put your code here
+}
