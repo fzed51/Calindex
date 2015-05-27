8db0daf7ad455f8e63b237813ef09a0a9da1bbea
fzed51 <fzed51@users.noreply.github.com>
2015-05-27 15:12:26 +0200

    :up: correction du problème de parsage de ligne multiple

--- app/controler/ICal/IcsParser.class.php
+++ app/controler/ICal/IcsParser.class.php
@@ -19,126 +19,126 @@ use stdClass;
  */
 class IcsParser {
 
-	/**
-	 * $data contien le contenu du fichier ics
-	 * @var array
-	 */
-	private $data = [];
+    /**
+     * $data contien le contenu du fichier ics
+     * @var array
+     */
+    private $data = [];
 
-	/**
-	 * $read_point position de lecture dans data
-	 * @var int
-	 */
-	private $read_point = 0;
+    /**
+     * $read_point position de lecture dans data
+     * @var int
+     */
+    private $read_point = 0;
 
-	/**
-	 * 	Résultat du parse
-	 * @var stdClass
-	 */
-	private $data_parsed = null;
+    /**
+     * 	Résultat du parse
+     * @var stdClass
+     */
+    private $data_parsed = null;
 
-	/**
-	 * __construct
-	 * @param type $filename
-	 */
-	public function __construct(/* string */$filename) {
-		$data_string = file_get_contents($filename);
-		$this->data = explode("\n", $data_string);
-		$this->parseIcs();
-	}
+    /**
+     * __construct
+     * @param type $filename
+     */
+    public function __construct(/* string */$filename) {
+        $data_string = file_get_contents($filename);
+        $this->data = explode("\n", $data_string);
+        $this->parseIcs();
+    }
 
-	/**
-	 * readData
-	 * Lit la ligne suivante de data
-	 * @return boolean|string
-	 */
-	private function readData() {
-		if ($this->read_point < count($this->data)) {
-			$line = $this->data[$this->read_point];
-			$this->read_point++;
-		} else {
-			$line = FALSE;
-		}
-		return $line;
-	}
+    /**
+     * readData
+     * Lit la ligne suivante de data
+     * @return boolean|string
+     */
+    private function readData() {
+        if ($this->read_point < count($this->data)) {
+            $line = $this->data[$this->read_point];
+            $this->read_point++;
+        } else {
+            $line = FALSE;
+        }
+        return $line;
+    }
 
-	/**
-	 * parseIcs
-	 * méthode de parse principale
-	 * @throws Exception
-	 */
-	private function parseIcs() {
-		$line = '';
-		while ('BEGIN:VCALENDAR' != trim($line = $this->readData())) {
-			if ($line === FALSE) {
-				throw new Exception("le fichier parser n'est pas valide\nLa balise 'BEGIN:VCALENDAR' était attendu.");
-			}
-			$line = $this->readData();
-		}
-		$this->data_parsed = $this->parseSubElement('VCALENDAR');
-	}
+    /**
+     * parseIcs
+     * méthode de parse principale
+     * @throws Exception
+     */
+    private function parseIcs() {
+        $line = '';
+        while ('BEGIN:VCALENDAR' != trim($line = $this->readData())) {
+            if ($line === FALSE) {
+                throw new Exception("le fichier parser n'est pas valide\nLa balise 'BEGIN:VCALENDAR' était attendu.");
+            }
+            $line = $this->readData();
+        }
+        $this->data_parsed = $this->parseSubElement('VCALENDAR');
+    }
 
-	/**
-	 *
-	 * @param stdClass $elementSuperieur
-	 * @param string $lastType
-	 * @return stdClass
-	 */
-	private function parseSubElement(/* string */ $lastType) {
-		$line = '';
-		switch ($lastType) {
-			case 'VCALENDAR':
-				$subElement = new Calendar();
-				break;
-			case 'VEVENT':
-				$subElement = new Event();
-				break;
-			case 'VTODO':
-				$subElement = new Todo();
-				break;
-			case 'VCard':
-				$subElement = new Card();
-				break;
-			default:
-				$subElement = new stdClass();
-				break;
-		}
+    /**
+     *
+     * @param stdClass $elementSuperieur
+     * @param string $lastType
+     * @return stdClass
+     */
+    private function parseSubElement(/* string */ $lastType) {
+        $line = '';
+        switch ($lastType) {
+            case 'VCALENDAR':
+                $subElement = new Calendar();
+                break;
+            case 'VEVENT':
+                $subElement = new Event();
+                break;
+            case 'VTODO':
+                $subElement = new Todo();
+                break;
+            case 'VCard':
+                $subElement = new Card();
+                break;
+            default:
+                $subElement = new stdClass();
+                break;
+        }
 
-		$lastKey = '__ERROR__';
-		while (false !== trim($line = $this->readData())) {
-			$line = rtrim($line);
-			if (1 === preg_match("/([^;:]+)(?:;([^:]*))?:(.+)/", $line, $output_array)) {
-				$key = $output_array[1];
-				$lastKey = $key;
-				$opt = $output_array[2];
-				$val = $output_array[3];
-				switch ($key) {
-					case 'BEGIN':
-						$lastType = $val;
-						$subElement->add($this->parseSubElement($lastType));
-						break;
-					case 'END':
-						return $subElement;
-					default :
-						switch ($opt) {
-							case 'VALUE=DATE':
-								$subElement->$key = new Date($val);
-								break;
-							default :
-								$subElement->$key = $val;
-						}
-				}
-			} else {
-				if ($line[0] === ' ') {
-					$subElement->$lastKey .= trim($line);
-				}
-			}
-		}
-		throw new Exception('Le sous element ne se termine pas');
-	}
+        $lastKey = '__ERROR__';
+        while (false !== trim($line = $this->readData())) {
+            $line = rtrim($line);
+            if (1 === preg_match("/([^;:]+)(?:;([^:]*))?:(.+)/", $line, $output_array)) {
+                $key = $output_array[1];
+                $lastKey = $key;
+                $opt = $output_array[2];
+                $val = $output_array[3];
+                switch ($key) {
+                    case 'BEGIN':
+                        $lastType = $val;
+                        $subElement->add($this->parseSubElement($lastType));
+                        break;
+                    case 'END':
+                        return $subElement;
+                    default :
+                        switch ($opt) {
+                            case 'VALUE=DATE':
+                                $subElement->$key = new Date($val);
+                                break;
+                            default :
+                                $subElement->$key = $val;
+                        }
+                }
+            } else {
+                if ($line[0] === ' ' || $line[0] === "\t") {
+                    $subElement->$lastKey .= trim($line);
+                }
+            }
+        }
+        throw new Exception('Le sous element ne se termine pas');
+    }
 
-	function getParse() {
-		return $this->data_parsed;
-	}
+    function getParse() {
+        return $this->data_parsed;
+    }
 
 }
